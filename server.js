17ef1bb19813bca3861d8b0c6a4e2c363816be76
Brian <40767783+brishaw@users.noreply.github.com>
2018-12-07 09:08:15 -0500

    cahnged mongoose connections for heroku...

--- server.js
+++ server.js
@@ -27,7 +27,10 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/allTheNewsdb", { useNewUrlParser: true });
+//mongoose.connect("mongodb://localhost/allTheNewsdb", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/allTheNewsdb";
+
+mongoose.connect(MONGODB_URI);
 
 // routes
 
